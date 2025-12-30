307388852e893ba8221a3ee34a4fab14fce24f1c
MorrisKehbel <morris.kehbel@gmail.com>
2025-12-30 19:48:03 +0100

    add advanced form with validation and adjusted styling (#8)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,11 +2,11 @@ import { FormWindow } from "@/components/index";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-dvh bg-blue-400 text-white w-full justify-center p-2">
-      <h1 className="mt-6 font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+    <main className="flex flex-col min-h-dvh text-white w-full justify-center p-2">
+      <h1 className="mt-6 font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl drop-shadow">
         Faire Miete oder Abzocke?
       </h1>
-      <h2 className="mt-4 text-center text-sm md:text-base lg:text-xl">
+      <h2 className="mt-4 text-center text-sm md:text-base lg:text-xl drop-shadow">
         Nutze modernstes Machine Learning, um den wahren Wert einer Wohnung zu
         ermitteln.
       </h2>
